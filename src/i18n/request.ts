21e3bc8c492cdc589411186fceda45c2e602409e
itswebday <info@itswebday.com>
2026-02-04 20:14:51 +0100

    Version 3.2

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,9 @@
-import { headers } from "next/headers";
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const localeHeader = (await headers()).get("x-next-intl-locale");
-  const requested = localeHeader ?? (await requestLocale);
+  const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
